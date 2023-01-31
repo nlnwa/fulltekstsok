eac08d2907d915f8dcb5b77574b84036e586c1ee
Magnus Breder Birkenes <magnus.birkenes@nb.no>
2023-01-31 17:25:33 +0100

    add config file with env variables

---
+++ webapp/config.py
@@ -0,0 +1,6 @@
+import os
+host=os.environ.get('POSTGRES_HOST')
+database=os.environ.get('POSTGRES_DB')
+user=os.environ.get('POSTGRES_USER')
+port=os.environ.get('POSTGRES_PORT')
+password=os.environ.get('POSTGRES_PASSWORD')
