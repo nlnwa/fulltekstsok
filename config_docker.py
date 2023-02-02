170c35d6d72122367149f3b4df10a2e2b3cd744e
Magnus Breder Birkenes <magnus.birkenes@nb.no>
2023-02-02 16:47:45 +0100

    update config_docker.py

---
+++ config_docker.py
@@ -0,0 +1,6 @@
+import os
+database=os.getenv("POSTGRES_DB")
+host=os.getenv("POSTGRES_HOST")
+port=os.getenv("POSTGRES_PORT")
+user=os.getenv("POSTGRES_USER")
+password=os.getenv("POSTGRES_PASSWORD")
