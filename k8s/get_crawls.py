2ac4db73698ad9cde1be32b42ad57900fd6ffb18
Magnus Breder Birkenes <magnus.birkenes@nb.no>
2023-02-02 16:27:15 +0000

    Update get_crawls.py

--- k8s/get_crawls.py
+++ k8s/get_crawls.py
@@ -11,8 +11,10 @@ crawl_id = 0
 for folder in index_folders:
     folder_path = os.path.join(input_folder, folder)
     for root,dirs,files in os.walk(folder_path):
+        # don't decend into folders containing screenshot or dns
         dirs[:] = [d for d in dirs if not re.findall('screenshot|dns', d.lower())]
 
+        # create new crawls for all folders on level 2 (statlige_2020, statlige_2021 etc.)
         if root[len(input_folder):].count(os.sep) == 2:
             crawl_name = root.split('/')[-1]
             crawl_id += 1
